79302a617381448e2a9e508ff96503b7eead17ae
Chris Byron <chris.byron@gmail.com>
2015-11-06 17:20:38 -0800

    Update objectify.js

--- objectify.js
+++ objectify.js
@@ -2,5 +2,5 @@ function GetAsJSON(jsonUrl){
 var Httpreq = new XMLHttpRequest(); // instantiate new request
 Httpreq.open("GET",jsonUrl,false);
 Httpreq.send(null);
-return JSON.parse(Httpreq.responseText); 
+return JSON.parse(Httpreq.responseText); // Parse string as object and return obj.
 }
